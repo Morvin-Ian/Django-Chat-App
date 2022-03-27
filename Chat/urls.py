6bc32aa3ae619e0601aa85867b1aa748b1447953
“Morvin-Ian” <morvinian@gmail.com>
2022-03-27 16:29:04 +0300

    deployment

--- Chat/urls.py
+++ Chat/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('update/<int:pk>', views.update_room, name='update-room'),
     path('delete/<int:pk>', views.delete_room, name='delete-room'),
     path('delete_message/<int:pk>', views.delete_message, name='delete-message'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('register/', views.register, name='register')
 ]
